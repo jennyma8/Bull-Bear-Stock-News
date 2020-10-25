a07a5b0b7986be29226c852cfb654a6acc9b50ef
jenny ma <jenny_lau_8@hotmail.com>
2020-10-24 22:54:50 -0400

    clean

--- client/src/components/StockPage/StockPage.js
+++ client/src/components/StockPage/StockPage.js
@@ -1,27 +1,17 @@
 import React from "react";
 import styled from "styled-components";
-import { useState, useEffect } from "react";
+
 import StockQuote from "../../components/StockQuote/StockQuote";
 import StockNews from "../StockNews/StockNews";
 import Search from "../../components/Search/Search";
 import Company from "../Company/Company";
 
-const StockPage = (props) => {
-  const [stockPage, setStockPage] = useState({});
-
-  useEffect(() => {
-    fetch("https://localhost:3000/stocks/" + props.match.params.ticker)
-      .then((res) => res.json())
-      .then((result) => {
-        setStockPage(result);
-      });
-  });
-  function changeStockPage(e) {}
+const StockPage = () => {
   return (
     <>
       <Wrapper>
         <Search />
-        <a href={"/stocks/" + ticker}>Edit</a>
+
         <StockWrapper>
           <QuoteWrapper>
             <StockQuote />
